f7f21f56099489c89b21ff6f223f20fa234f895c
Dmytro Kulak <mail@dmytrokulak.net>
2020-08-03 09:34:14 +0300

    user lessons view

--- sw/src/actions/lessonActions.js
+++ sw/src/actions/lessonActions.js
@@ -1,6 +1,28 @@
 import { GET_LESSONS, UPDATE_LESSON, LESSON_ERROR } from './types';
 
 //Get lessons from server
+export const getLessonsByUser = (id) => async (dispatch) => {
+  try {
+    const res = await fetch('/lessons?userId=' + id);
+    const data = await res.json();
+    for (let i = 0; i < data.length; i++) {
+      data[i].user = await (await fetch('/users/' + data[i].userId)).json();
+      data[i].lessonType = await (await fetch('/lesson-types/' + data[i].lessonTypeId)).json();
+      data[i].offer = data[i].lessonType.offers.filter((o) => o.id === data[i].offerId)[0];
+    }
+    dispatch({
+      type: GET_LESSONS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LESSON_ERROR,
+      payload: error,
+    });
+  }
+};
+
+//Get lessons from server
 export const getLessons = () => async (dispatch) => {
   try {
     const res = await fetch('/lessons');
--- sw/src/components/user/UserLessons.js
+++ sw/src/components/user/UserLessons.js
@@ -1,11 +1,117 @@
-import React, { Fragment } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getLessonsByUser, updateLesson } from '../../actions/lessonActions';
+import Preloader from '../layout/Preloader';
+import LessonItem from '../admin/lessons/LessonItem';
 
-const UserLessons = () => {
+const UserLessons = ({ lessons: { collection, loading }, auth: { user }, getLessonsByUser, updateLesson }) => {
+  const [requested, setRequested] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [completed, setCompleted] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      getLessonsByUser(user.id);
+    }
+    //eslint-disable-next-line
+  }, [user]);
+
+  useEffect(() => {
+    let req = [];
+    let upc = [];
+    let com = [];
+    for (let i = 0; collection && i < collection.length; i++) {
+      if (collection[i].status === 'requested') {
+        req.push(collection[i]);
+      } else if (collection[i].status === 'upcoming') {
+        upc.push(collection[i]);
+      } else if (collection[i].status === 'completed') {
+        com.push(collection[i]);
+      }
+    }
+    setRequested(req);
+    setUpcoming(upc);
+    setCompleted(com);
+  }, [collection]);
+
+  const updateLessonStatus = (id, status) => {
+    const item = collection[id];
+    item.status = status;
+    updateLesson(item);
+  };
+
   return (
-    <Fragment>
+    <div id='section-lessons' className='section'>
       <h4 className='center-align'>Lessons</h4>
-    </Fragment>
+      {loading || collection == null ? (
+        <Preloader />
+      ) : (
+        <Fragment>
+          {requested.length > 0 && (
+            <Fragment>
+              <blockquote>Requested</blockquote>
+              <ul className='collection'>
+                {requested.map((item) => (
+                  <li key={item.id} className='collection-item'>
+                    <LessonItem item={item} />
+                    <div className='action-buttons'>
+                      {/* <a href='#!' className='waves-effect waves-light grey lighten-3 btn-flat '>
+                        Decline
+                      </a>
+                      <a
+                        href='#!'
+                        onClick={() => updateLessonStatus(item.id - 1, 'upcoming')}
+                        className='waves-effect waves-light teal white-text btn-flat'
+                      >
+                        Accept
+                      </a> */}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </Fragment>
+          )}
+          {upcoming.length > 0 && (
+            <Fragment>
+              <blockquote>Upcoming</blockquote>
+              <ul className='collection'>
+                {upcoming.map((item) => (
+                  <li key={item.id} className='collection-item'>
+                    <LessonItem item={item} />
+                    <div className='action-buttons'></div>
+                  </li>
+                ))}
+              </ul>
+            </Fragment>
+          )}
+          {completed.length > 0 && (
+            <Fragment>
+              <blockquote>Completed</blockquote>
+              <ul className='collection'>
+                {completed.map((item) => (
+                  <li key={item.id} className='collection-item'>
+                    <LessonItem item={item} />
+                    <div className='action-buttons'></div>
+                  </li>
+                ))}
+              </ul>
+            </Fragment>
+          )}
+        </Fragment>
+      )}
+    </div>
   );
 };
 
-export default UserLessons;
+UserLessons.propTypes = {
+  lessons: PropTypes.object.isRequired,
+  getLessonsByUser: PropTypes.func.isRequired,
+  updateLesson: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  lessons: state.lessons,
+  auth: state.auth,
+});
+export default connect(mapStateToProps, { getLessonsByUser, updateLesson })(UserLessons);
